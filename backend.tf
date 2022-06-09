9e9ce986cdcfdc51c23f9dcbbcb26913d9d2e1f3
arizona365 <mkeskin.az@gmail.com>
2022-06-08 17:59:32 -0700

    Created new repo for terraform and ansible

---
+++ backend.tf
@@ -0,0 +1,14 @@
+# And that AWS user being used by TF has read/write perms
+terraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "terraformstatebucket1453"
+  }
+}
+
---
+++ outputs.tf
@@ -0,0 +1,16 @@
+###This chunk of template can also be put inside outputs.tf for better segregation 
+output "Jenkins-Main-Node-Public-IP" {
+  value = aws_instance.jenkins-master.public_ip
+}
+
+output "Jenkins-Worker-Public-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.public_ip
+  }
+}
+
+output "LB-DNS-NAME" {
+  value = aws_lb.application-lb.dns_name
+}
+
